c40f31f8182ab605cce9f5c471fe91ba13c728c2
sharianakram <sharianakram0503@gmail.com>
2024-09-30 09:58:26 -0400

    Fixed issue by converting NextLine into an int, Completed Mod3 - Excercise 3 - Basic Calculator

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -4,27 +4,31 @@ public class Calculator {
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter the first number: ");
-        int firstNumber = myScanner.nextInt();
+        int firstNumber = Integer.parseInt(myScanner.nextLine());
         System.out.println("Enter the second number: ");
-        int secondNumber = myScanner.nextInt();
+        int secondNumber = Integer.parseInt(myScanner.nextLine());
         System.out.println("Possible calculations: ");
         System.out.println("(A)dd");
         System.out.println("(S)ubtract");
         System.out.println("(M)ultiply");
         System.out.println("(D)ivide");
         System.out.println("Please select an option by entering the capital letter that corresponds to the action: ");
-        String opertaion = myScanner.nextLine();
-        System.out.println(opertaion);
+        String operation = myScanner.nextLine();
 
-        if (opertaion == "A"){
+        if (Objects.equals(operation, "A")){
             int addValue = firstNumber + secondNumber;
             System.out.println(firstNumber + " + " +  secondNumber + " = " + addValue);
-        } else if (opertaion == "S"){
+        } else if (Objects.equals(operation, "S")){
             int subtractValue = firstNumber - secondNumber;
-        } else if (opertaion == "M"){
+            System.out.println(firstNumber + " - " +  secondNumber + " = " + subtractValue);
+        } else if (Objects.equals(operation, "M")){
             int multiplyValue = firstNumber * secondNumber;
-        } else {
+            System.out.println(firstNumber + " * " +  secondNumber + " = " + multiplyValue);
+        } else if (Objects.equals(operation, "D")){
             double divideValue = firstNumber / secondNumber;
+            System.out.println(firstNumber + " / " +  secondNumber + " = " + divideValue);
+        }else{
+            System.out.println("invalid option");
         }
 
 
